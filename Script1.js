5d886b81d4154ee10d8325de40b21cdc4adeffc2
Jordan Swanson <j_swanson2@student.fhsu.edu>
2024-06-20 17:45:51 -0500

    question one finished

--- Script1.js
+++ Script1.js
@@ -1,1 +1,13 @@
-// JavaScript source code
+const answer = document.getElementById("guessHeader");
+let x = Math.floor(Math.random() * 10);
+function numberGuess() {
+    
+    let guessInput = document.getElementById("guess").value;
+    if (guessInput == x) {        
+        answer.textContent = "You got the answer!";
+    }
+    else {
+        answer.textContent = "Incorrect Guess! Try again";
+    }
+            
+}
