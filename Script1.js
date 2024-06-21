39017c9479422f139367c3af1290aa55c92c186d
Jordan Swanson <j_swanson2@student.fhsu.edu>
2024-06-20 21:44:22 -0500

    2 finished

--- Script1.js
+++ Script1.js
@@ -1,13 +1,36 @@
-const answer = document.getElementById("guessHeader");
-let x = Math.floor(Math.random() * 10);
+
 function numberGuess() {
-    
-    let guessInput = document.getElementById("guess").value;
-    if (guessInput == x) {        
-        answer.textContent = "You got the answer!";
-    }
-    else {
-        answer.textContent = "Incorrect Guess! Try again";
+    const answer = document.getElementById("guessHeader");
+    let x = Math.floor(Math.random() * 10);
+    let running = true;
+
+    while (running) {
+        let guessInput = prompt("Enter a number")//document.getElementById("guess").value;
+        if (guessInput == x) {
+            answer.textContent = "You got the answer! It was: " + x;
+            running = false;
+        }
     }
             
 }
+
+function passwordSet() {
+    let password = prompt("Enter a password"); //initial prompt
+    let running = true;
+    do {
+
+        let checkPassword = prompt("Confirm Password");
+
+        if (password == checkPassword) {
+            running = false;
+        }
+        else {
+            password = prompt("Password mismatch, please start over with a new Password");
+        }
+    }
+    while (running)
+
+    document.getElementById("confirmPrompt").textContent = "Password set succesful!";
+}
+
+
